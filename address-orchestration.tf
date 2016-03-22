1e4a67b07d6a2577065ac2ace22ff6a1ae0619f2
Adron Hall QC <adron_hall@homedepot.com>
2016-03-21 21:50:24 -0700

    recipe-static-ip-assigned-to-instance.

---
+++ address-orchestration.tf
@@ -0,0 +1,3 @@
+resource "google_compute_address" "orchestration-address" {
+    name = "orchestration-address"
+}
--- instance-orchestration.tf
+++ instance-orchestration.tf
@@ -13,11 +13,9 @@ resource "google_compute_instance" "orchestration" {
     network_interface {
         network = "default"
         access_config {
-            // Ephemeral IP
+             nat_ip = "${google_compute_address.orchestration-address.address}"
         }
     }
-
-    metadata_startup_script = "echo hi > ~/test.txt"
 
     service_account {
         scopes = ["userinfo-email", "compute-ro", "storage-ro"]
